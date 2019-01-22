18f1eedf7792b1a401155a09c55147750c23ce11
kpnarola <kpnarola@gmail.com>
2019-01-22 10:52:37 +0000

    email a friend product

--- src/test/java/nopcommercewithcucumber/RunTest.java
+++ src/test/java/nopcommercewithcucumber/RunTest.java
@@ -4,7 +4,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ".",tags = "@sortBy,  @termsAndCondition,  @buyProduct, @emailAProduct, @canNotEmailAProduct, @registration")
+@CucumberOptions(features = ".",tags = "@sortBy,  @termsAndCondition,  @buyProduct, " +
+        "@emailAProduct, @canNotEmailAProduct, @registration",
+format = {"pretty","html://target/cucumber-reports"})
 
 public class RunTest {
 
